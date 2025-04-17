031c154c6970b5995cbbd64f3a86fb407a07a345
Bishrav Shiwakoti <bishravs@gmail.com>
2025-04-17 11:26:07 +0545

    changed to inputsjava

---
+++ Inputs.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the name");
+        String str = sc.next();
+        System.out.println(str); 
+    }
+}
