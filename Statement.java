a3f0fe528c2bf8bf178a52135639bdb21cc48cbf
Bishrav Shiwakoti <bishravs@gmail.com>
2025-04-21 10:32:01 +0545

    learned about the scanner with if else statemet

--- Statement.java
+++ Statement.java
@@ -1,7 +1,14 @@
+
+import java.util.Scanner;
+
 public class Statement {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number for x");
+        int x = sc.nextInt();
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("enter the number for y");
+        int y = sc.nextInt();
         if (x / y == 2){
             System.out.println("that is divisible");
         }
