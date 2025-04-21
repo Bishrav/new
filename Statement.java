bd642faeeb8a9fc7daa0c0c0a6aad68cae069292
Bishrav Shiwakoti <bishravs@gmail.com>
2025-04-21 10:24:02 +0545

    learned about if statement

---
+++ Statement.java
@@ -0,0 +1,9 @@
+public class Statement {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        if (x / y == 2){
+            System.out.println("that is divisible");
+        }
+    }
+}
