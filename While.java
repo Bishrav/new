c1d021d4af9fb8d59df4dd26dfdb88062cb413ce
Bishrav Shiwakoti <bishravs@gmail.com>
2025-04-29 09:58:20 +0545

    added the infinite loop

---
+++ While.java
@@ -0,0 +1,12 @@
+
+
+public class While {
+    public static void main(String[] args) {
+        int number = 1;
+        while(number <=5){
+            System.out.println("Hello");
+            number++;
+        }
+        System.out.print("thats all");
+    }
+}
