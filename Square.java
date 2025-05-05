7d6afbc15f9e080cacc2d2870551900a73865dd6
Bishrav Shiwakoti <bishravs@gmail.com>
2025-05-05 11:02:23 +0545

    learned square root

---
+++ Square.java
@@ -0,0 +1,12 @@
+public class Square {
+    public static int getSquare(int x) {
+        return x * x;
+        
+    }
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++){
+            int result = getSquare(i);
+            System.out.println("Square of"+i+"is:"+result);
+        }
+    }
+}
